567b0a02ed133067581511721e4d3f57eb1dbc91
klot3n <klot3n@gmail.com>
2019-10-24 21:27:19 +0500

    end of chapter twelve

--- app/src/main/java/com/hfad/bitsandpizzas/OrderActivity.kt
+++ app/src/main/java/com/hfad/bitsandpizzas/OrderActivity.kt
@@ -2,8 +2,11 @@ package com.hfad.bitsandpizzas
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
+import android.view.View
+import android.widget.Toast
 
 
 class OrderActivity: AppCompatActivity() {
@@ -14,6 +17,16 @@ class OrderActivity: AppCompatActivity() {
         setSupportActionBar(toolbar)
         val actionBar=supportActionBar
         actionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    fun onClickDone(view:View){
+        val text="Your order has been updated"
+        val duration = Snackbar.LENGTH_SHORT
+        val snackbar = Snackbar.make(findViewById(R.id.coordinator),text,duration)
+        snackbar.setAction("Undo",View.OnClickListener {
+            val toast = Toast.makeText(this, "Undone!",Toast.LENGTH_SHORT).show()
+        })
+        snackbar.show()
     }
 
 
